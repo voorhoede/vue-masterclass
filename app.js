f0ffa31003aa5bcb43b1213f1619e3afaaa2fef0
Remco <remco@voorhoede.nl>
2018-03-06 13:03:31 +0100

    feat: exercise1

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+    Define your components here.
+
+    Example:
+
+    Vue.component('message-list', {
+
+    })
+*/
+
+const app = new Vue({
+    el : '.app',
+
+    //Uncomment the next code and list your components inside the app div
+
+    /* template : `<div class="app app--searching">
+    
+    </div>` */
+});
+
+Vue.config.devtools = true;
