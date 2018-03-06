6653690cb5828ed97a20aa464c9f07844e776c2c
Remco <remco@voorhoede.nl>
2018-03-06 15:05:08 +0100

    feat: exercise 5 solution

--- components/user-profile.js
+++ components/user-profile.js
@@ -1,8 +1,12 @@
 Vue.component('user-profile', {
+    data : function () {
+        return window.chat;
+    },
+
     template : `
         <div class="user-profile">
             <label class="user-profile__label" for="name">Name</label>
-            <input type="text" id="name" value="Anoniempje" class="reset-text user-profile__name">
+            <input type="text" id="name" v-model="user.name" class="reset-text user-profile__name">
         </div>
     `
 });
