f30017a2dabf9cf749c4e8ce02c374b639c56db2
Remco <remco@voorhoede.nl>
2018-03-06 15:39:23 +0100

    feat: exercise 6 solution

--- components/search-sidebar.js
+++ components/search-sidebar.js
@@ -6,8 +6,17 @@ Vue.component('search-sidebar', {
     template : `
         <aside class="search-sidebar" v-if="searching">
             <button class="reset-button search-sidebar__close" v-on:click="searching = false" aria-label="Close" title="Close"></button>
-            <h2 class="search-sidebar__header">0 Messages</h2>
-            <!--<message-list></message-list>-->
+            <h2 class="search-sidebar__header">{{ header }}</h2>
+            <message-list v-bind:messages="filteredMessages"></message-list>
         </aside>
-    `
+    `,
+
+    computed : {
+        filteredMessages() {
+            return this.messages.filter(message => message.text.indexOf(this.searchText) > -1);
+        },
+        header() {
+            return `${this.filteredMessages.length} Message` + (this.filteredMessages.length !== 1 ? 's' : '');
+        }
+    }
 });
