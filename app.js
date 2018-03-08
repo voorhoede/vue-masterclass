c2e3e7b1971c27286ac21d70dc032de1d709714e
Remco <remco@voorhoede.nl>
2018-03-08 09:36:23 +0100

    feat: add readme

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ import './components/search-form.js';
 import './components/search-sidebar.js';
 import './components/user-profile.js';
 
-/*
-    Look at the ui in the browser and determine the data structure for your slack clone.
-    Make the data "reactive".
-
-    Try the following things:
-    - Add or remove an item from the message array. Either through the browser console or the devtools
-    - Change an item in the message array. What happens?
-    - Add a new property called 'newProp' through the browser console. What happens?
-*/
-
+// define your data structure here
 window.chat = {
     messages : []
 }
@@ -27,10 +18,12 @@ window.app = new Vue({
     el: '.app',
 
     watch : {
+        //try to add the newProp property to window.chat. Does the watcher get triggered?
         newProp() {
             console.log('newProp was added/changed');
         },
 
+        //try to change the messages
         messages(newValue, oldValue) {
             console.log(`messages changed to ${newValue}`);
         }
