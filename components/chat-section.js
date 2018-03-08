3fd19e468468aa91aa60f9e5a3f17d65889d1f63
Remco <remco@voorhoede.nl>
2018-03-08 10:41:55 +0100

    fix: remove unneeded code

--- components/chat-section.js
+++ components/chat-section.js
@@ -8,18 +8,7 @@ Vue.component('chat-section', {
             <div class="chat-section__messages">
                 <message-list v-bind:messages="messages"></message-list>
             </div> 
-            <message-field v-on:submit="onSubmit" class="chat-section__field"></message-field>
+            <message-field class="chat-section__field"></message-field>
         </section>
-    `,
-
-    methods : {
-        onSubmit(text) {
-            this.messages.push({
-                id : this.messages.length,
-                date : new Date(),
-                user : this.user,
-                text,
-            });
-        }
-    }
+    `
 });
