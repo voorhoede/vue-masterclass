e5860710a78a939f0d0f11ee492992c9e8c2dce0
Remco <remco@voorhoede.nl>
2018-03-08 10:46:49 +0100

    refactor: removed solution from exercise

--- components/chat-section.js
+++ components/chat-section.js
@@ -8,18 +8,18 @@ Vue.component('chat-section', {
             <div class="chat-section__messages">
                 <message-list v-bind:messages="messages"></message-list>
             </div> 
-            <message-field v-on:submit="onSubmit" class="chat-section__field"></message-field>
+            <message-field class="chat-section__field"></message-field>
         </section>
     `,
 
     methods : {
-        onSubmit(text) {
-            this.messages.push({
-                id : this.messages.length,
+        createMessage(text) {
+            return {
+                text, 
                 date : new Date(),
                 user : this.user,
-                text,
-            });
+                id : this.messages.length
+            }
         }
     }
 });
