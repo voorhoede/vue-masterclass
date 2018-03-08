a769482d72c2a27dd545aa55b9dbeb766be2e5b0
Remco <remco@voorhoede.nl>
2018-03-08 09:43:16 +0100

    feat: add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Exercise 3
+
+- Show “there are no messages” when the message array is empty.
+- Render a message for each item in the message array.
--- app.js
+++ app.js
@@ -9,16 +9,6 @@ import './components/search-form.js';
 import './components/search-sidebar.js';
 import './components/user-profile.js';
 
-/*
-    Look at the ui in the browser and determine the data structure for your slack clone.
-    Make the data "reactive".
-
-    Try the following things:
-    - Add or remove an item from the message array. Either through the browser console or the devtools
-    - Change an item in the message array. What happens?
-    - Add a new property called 'newProp' through the browser console. What happens?
-*/
-
 window.chat = {
     user : {
         name : "Anoniempje",
